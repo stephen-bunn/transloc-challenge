a53f2153af6af2853f8443498aa492144c396ab4
Stephen Bunn <stephen@bunn.io>
2019-06-18 14:39:29 -0400

    Componentizing out the gradient component
    Signed-off-by: Stephen Bunn <stephen@bunn.io>

---
+++ src/components/GradientBuilder.vue
@@ -0,0 +1,94 @@
+<template lang="pug">
+  v-layout(column).gradient-builder
+    template(v-for="(entry, entryIdx) in gradient")
+      v-menu(offset-y :key="`gradient-entry-${entryIdx}`" :close-on-content-click="false" :z-index="999" v-model="entry.menu")
+        template(v-slot:activator="{ on }")
+          v-slider(thumb-label :color="entry.color" v-model="entry.value" prepend-icon="fe-edit-2" append-icon="fe-x-circle" @input="(value) => onValueUpdated(entryIdx, value)" @click:prepend="entry.menu = true" @click:append="() => removeGradient(entryIdx)")
+        color-picker(:value="{hex: `${entry.color}`}" @input="(value) => onColorUpdated(entryIdx, value)")
+    v-flex.justify-end
+      v-btn(outline block color="primary" @click="addGradient")
+        span Add Color
+</template>
+
+<script lang="ts">
+import _ from "lodash"
+import { Component, Watch, Vue } from "vue-property-decorator"
+import { Chrome as ChromeColor } from "vue-color"
+
+export interface Gradient {
+  value: number
+  color: string
+  menu: boolean
+}
+
+interface ColorValue {
+  a: number
+  hex: string
+  hex8: string
+  hsl: {
+    a: number
+    h: number
+    s: number
+    l: number
+  }
+  hsv: {
+    a: number
+    h: number
+    s: number
+    v: number
+  }
+  oldHue: number
+  rgba: {
+    a: number
+    r: number
+    g: number
+    b: number
+  }
+  source: string
+}
+
+const defaultGradient: Gradient[] = [
+  { value: 15, color: "#00E5FF", menu: false },
+  { value: 25, color: "#76FF03", menu: false },
+  { value: 30, color: "#FF3D00", menu: false },
+]
+
+@Component({ name: "gradient-builder", components: { "color-picker": ChromeColor } })
+export default class GradientBuilder extends Vue {
+  public gradient: Gradient[] = defaultGradient
+
+  public async mounted() {
+    this.$emit("change", this.gradient)
+  }
+
+  public onColorUpdated(entryIdx: number, value: ColorValue) {
+    this.gradient[entryIdx].color = value.hex
+  }
+
+  public onValueUpdated(entryIdx: number, value: number) {
+    _.forEach(_.range(entryIdx), lowIdx => {
+      if (this.gradient[lowIdx].value > value) {
+        this.gradient[lowIdx].value = value
+      }
+    })
+    _.forEach(_.range(entryIdx + 1, _.size(this.gradient)), highIdx => {
+      if (this.gradient[highIdx].value < value) {
+        this.gradient[highIdx].value = value
+      }
+    })
+  }
+
+  public removeGradient(entryIdx: number) {
+    this.gradient.splice(entryIdx, 1)
+  }
+
+  public addGradient() {
+    this.gradient.push({ value: 100, color: "#000000", menu: false })
+  }
+
+  @Watch("gradient", { deep: true })
+  private onGradientUpdated(value: Gradient[]) {
+    this.$emit("change", value)
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,111 +16,37 @@
         v-card.mt-4
           v-card-title.title Gradient
           v-card-text
-            v-menu(offset-x :close-on-content-click="false" :z-index="999" :nudge-right="8" v-model="lowMenu")
-              template(v-slot:activator="{ on }")
-                v-slider(thumb-label :color="lowColor" v-model="lowGradient" label="Low" append-icon="fe-edit-2" @click:append="lowMenu = true")
-              chrome-color(:value="lowColorPicker" @input="onLowColorUpdated")
+            gradient-builder(@change="onGradientUpdated")
 
-            v-menu(offset-x :close-on-content-click="false" :z-index="999" :nudge-right="8" v-model="medMenu")
-              template(v-slot:activator="{ on }")
-                v-slider(thumb-label :color="medColor" v-model="medGradient" label="Med" append-icon="fe-edit-2" @click:append="medMenu = true")
-              chrome-color(:value="medColorPicker" @input="onMedColorUpdated")
-
-            v-menu(offset-x :close-on-content-click="false" :z-index="999" :nudge-right="8" v-model="highMenu")
-              template(v-slot:activator="{ on }")
-                v-slider(thumb-label :color="highColor" v-model="highGradient" label="High" append-icon="fe-edit-2" @click:append="highMenu = true")
-              chrome-color(:value="highColorPicker" @input="onHighColorUpdated")
     v-flex.xs10
       heat-map(:blur="blur" :radius="radius" :minOpacity="opacity" :gradient="gradient")
 </template>
 
 <script lang="ts">
+import _ from "lodash"
 import { Component, Watch, Vue } from "vue-property-decorator"
-import { Chrome as ChromeColor } from "vue-color"
 
+import GradientBuilder, { Gradient } from "@/components/GradientBuilder.vue"
 import HeatMap, { HeatMapGradient } from "@/components/HeatMap.vue"
 
 @Component({
   components: {
     HeatMap,
-    "chrome-color": ChromeColor,
+    GradientBuilder,
   },
 })
 export default class Home extends Vue {
   public blur: number = 12
   public radius: number = 25
   public minOpacity: number = 10
+  public gradient: HeatMapGradient = {}
 
-  public lowGradient: number = 15
-  public lowColor: string = "#00E5FF"
-  public lowMenu: boolean = false
-  public medGradient: number = 25
-  public medColor: string = "#76FF03"
-  public medMenu: boolean = false
-  public highGradient: number = 30
-  public highColor: string = "#FF3D00"
-  public highMenu: boolean = false
-
-  get lowColorPicker() {
-    return {
-      hex: this.lowColor,
-    }
-  }
-
-  get medColorPicker() {
-    return {
-      hex: this.medColor,
-    }
-  }
-
-  get highColorPicker() {
-    return {
-      hex: this.highColor,
-    }
-  }
-
-  get gradient(): HeatMapGradient {
-    return {
-      [this.lowGradient / 100.0]: this.lowColor,
-      [this.medGradient / 100.0]: this.medColor,
-      [this.highGradient / 100.0]: this.highColor,
-    }
-  }
-
-  @Watch("highGradient")
-  private onHighGradientUpdated(value: number) {
-    if (value < this.medGradient) {
-      this.medGradient = value
-    }
-  }
-
-  private onLowColorUpdated(value: object) {
-    this.lowColor = value.hex
-  }
-
-  private onMedColorUpdated(value: object) {
-    this.medColor = value.hex
-  }
-
-  private onHighColorUpdated(value: object) {
-    this.highColor = value.hex
-  }
-
-  @Watch("medGradient")
-  private onMedGradientUpdated(value: number) {
-    if (value < this.lowGradient) {
-      this.lowGradient = value
-    }
-    if (value > this.highGradient) {
-      this.highGradient = value
-    }
-  }
-
-  @Watch("lowGradient")
-  private onLowGradientUpdated(value: number) {
-    if (value > this.medGradient) {
-      this.medGradient = value
-    }
+  private onGradientUpdated(value: Gradient[]) {
+    let gradient: HeatMapGradient = {}
+    _.forEach(value, entry => {
+      gradient[entry.value / 100.0] = entry.color
+    })
+    this.gradient = gradient
   }
 
   get opacity(): number {
